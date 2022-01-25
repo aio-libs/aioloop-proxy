fc9864194897b107128de2817ef2aa8d47690f16
Andrew Svetlov <andrew.svetlov@gmail.com>
2022-01-26 00:51:42 +0200

    Don't finalize closed loop (#8)

--- aioloop_proxy/_api.py
+++ aioloop_proxy/_api.py
@@ -12,11 +12,15 @@ def proxy(loop, *, kind=CheckKind.ALL):
     try:
         yield proxy_loop
     finally:
-        proxy_loop.run_until_complete(proxy_loop.shutdown_default_executor())
-        proxy_loop.run_until_complete(proxy_loop.check_and_shutdown())
-        if loop.get_debug() != debug:
-            loop.set_debug(debug)
-        if loop.get_exception_handler() != exception_handler:
-            loop.set_exception_handler(exception_handler)
-        if loop.slow_callback_duration != slow_callback_duration:
-            loop.slow_callback_duration = slow_callback_duration
+        if proxy_loop.is_closed():
+            return
+        if not loop.is_closed():
+            proxy_loop.run_until_complete(proxy_loop.shutdown_default_executor())
+            proxy_loop.run_until_complete(proxy_loop.check_and_shutdown())
+            if loop.get_debug() != debug:
+                loop.set_debug(debug)
+            if loop.get_exception_handler() != exception_handler:
+                loop.set_exception_handler(exception_handler)
+            if loop.slow_callback_duration != slow_callback_duration:
+                loop.slow_callback_duration = slow_callback_duration
+        proxy_loop.close()
